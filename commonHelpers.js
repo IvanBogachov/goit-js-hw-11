21204a14d2dceb4443e6fbb85d54b5709bd32b02
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-08-12 16:07:57 +0000

    Deploying to gh-pages from @ IvanBogachov/goit-js-hw-11@02369a45a6014055a212b2294c28f5b40a067f7f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,11 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l=document.querySelector(".btn"),u=document.querySelector(".gallery"),a=new URLSearchParams({_limit:9,_sort:"id"}),i=`https://jsonplaceholder.typicode.com/photos?${a}`;l.addEventListener("click",()=>{f().then(r=>d(r)).catch(r=>console.log(r))});console.log(i);function f(){return fetch(i).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function d(r){const o=r.map(s=>`
+        <li class="gallery-item">
+        <a class="gallery-link" href="${s.url}">
+        <img class="gallery-image"
+        src="${s.thumbnailUrl}"
+        alt="${s.url}"
+        />
+        </a>
+        </li>
+      `).join("");u.insertAdjacentHTML("beforeend",o)}
 //# sourceMappingURL=commonHelpers.js.map
